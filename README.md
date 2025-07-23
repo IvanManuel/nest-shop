b949239c452b6cb70f11caebe8af302b5459f83c
IvanManuel <mendozazivan@gmail.com>
2025-07-23 22:43:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
+</p>
+
+# Teslo API
+
+1. Clonar el repositorio
+
+2. Ejecutar
+```
+yarn install
+```
+
+3. Clona el archivo __.env.template__ y renombrar a __.env__
+
+4. Cambiar las variables de entorno
+
+5. Levantar la base de datos
+```
+docker-compose up -d
+```
+
+6. Levantar el proyecto
+```
+yarn start:dev
+```
---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: +process.env.DB_PORT || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME || 'teslo-shop',
+      autoLoadEntities: true,
+      synchronize: true, // Note: Set to false in prod
+    }),
+
+    ProductsModule,
+
+    CommonModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
